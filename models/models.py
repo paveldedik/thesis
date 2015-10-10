Fixed filter open_questions, added new metric log-likelihood, improoved randomized factor in staircase search function, adjusted default parameters in models.

models/models.py
@@ -634,10 +634,10 @@ class PFAGong(PFAModel):
     """
 
     def __init__(self, *args, **kwargs):
-        kwargs.setdefault('gamma', 1.5)
-        kwargs.setdefault('delta', -0.1)
+        kwargs.setdefault('gamma', 2.1)
+        kwargs.setdefault('delta', -0.8)
 
-        self.decay = kwargs.pop('decay', 0.9)
+        self.decay = kwargs.pop('decay', 0.8)
 
         super(PFAGong, self).__init__(*args, **kwargs)
 
@@ -710,7 +710,7 @@ class PFAForgetting(PFAGong):
     """
 
     def __init__(self, *args, **kwargs):
-        time_effect = lambda t: 1.4 - 0.1*np.log(t)
+        time_effect = lambda t: 1.1 - 0.075 * np.log(t)
         self.time_effect = kwargs.pop('time_effect_fun', time_effect)
 
         kwargs.setdefault('gamma', 1.7)

models/optimize.py
@@ -685,7 +685,8 @@ class GradientDescent(object):
             self.log_staircase = kwargs.pop('log_staircase', False)
 
             self.metadata = {}
-            self.seed = kwargs.pop('seed')
+            self.random_factor = kwargs.pop('random_factor')
+            self.random_chance = kwargs.pop('random_chance', 1000)
 
             if self.log_metadata:
                 self.metadata['diffs'] = []
@@ -717,11 +718,17 @@ class GradientDescent(object):
             self.gamma += self.learn_rate * shift * item.gamma_effect
             self.delta += self.learn_rate * shift * item.delta_effect
 
-            if self.seed is not None:
-                if not np.random.randint(1000):
-                    self.gamma += np.random.uniform(-self.seed, self.seed)
-                if not np.random.randint(1000):
-                    self.delta += np.random.uniform(-self.seed, self.seed)
+            if self.random_factor is not None:
+                factor = self.random_factor
+                chance = self.random_chance
+
+                if not np.random.randint(chance):
+                    self.gamma += np.random.uniform(-factor, factor)
+                if not np.random.randint(chance):
+                    self.delta += np.random.uniform(-factor, factor)
+                if has_practices and not np.random.randint(chance):
+                    self.staircase[seconds] += \
+                        np.random.uniform(-factor, factor)
 
             if answer.is_correct:
                 item.inc_knowledge(self.gamma * shift)
@@ -753,7 +760,7 @@ class GradientDescent(object):
 
     def search(self, model_fun, init_parameters,
                init_learn_rate=0.01, number_of_iter=10, log_metadata=True,
-               echo_iterations=True, init_seed=None):
+               echo_iterations=True, random_factor=None, random_chance=None):
         """Finds optimal parameters for given model.
 
         :param model_fun: Callable that trains the model using the given
@@ -777,10 +784,12 @@ class GradientDescent(object):
 
         for i in range(1, number_of_iter + 1):
             model_kwargs = {
-                'seed': init_seed / (i ** 2) if init_seed else None,
                 'learn_rate': init_learn_rate / (i / 2),
                 'log_metadata': log_metadata,
                 'log_staircase': i == number_of_iter,
+                'random_factor': (random_factor / (i ** 2)
+                                  if random_factor else None),
+                'random_chance': random_chance or 1000,
             }
             model_kwargs.update(parameters[i-1])
 